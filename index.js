a7c7baaf03b6ace07b0f138aeaa9a8c3420b547f
Duraan Ali <duraan522@gmail.com>
2022-04-11 11:56:43 -0500

    Heroku ready

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+require('dotenv').config();
+
 const server = require('./api/server.js');
 
-const port = 4000;
+// DEVELOPMENT ENVIRONMENT
+// PRODUCTION ENVIRONMENT
+
+// ENVIRONMENT VARIABLES - STATIC AND DYNAMIC
+
+const port = process.env.PORT | 4000
+
+console.log(`Listening on port ${port}`);
 
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
